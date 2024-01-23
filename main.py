3998a3ddbaedc523ade3e97b95ff6a74c8155b1d
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2024-01-23 17:16:24 +0100

    Working implementation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from naiveBayesClassifier import NaiveBayesClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from ucimlrepo import fetch_ucirepo
 iris = fetch_ucirepo(id=53)
 
@@ -12,11 +12,29 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 X_train = X_train.values
 X_test = X_test.values
 y_train = y_train['class'].values
-y_test = y_test.values
+y_test = y_test['class'].values
 
 classifier = NaiveBayesClassifier()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
-accuracy_custom = accuracy_score(y_test, y_pred)
-print(f'Accuracy (Custom Naive Bayes): {accuracy_custom}')
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.4f}")
+
+# Precision
+precision = precision_score(y_test, y_pred, average='weighted')
+print(f"Precision: {precision:.4f}")
+
+# Recall
+recall = recall_score(y_test, y_pred, average='weighted')
+print(f"Recall: {recall:.4f}")
+
+# F1 Score
+f1 = f1_score(y_test, y_pred, average='weighted')
+print(f"F1 Score: {f1:.4f}")
+
+# Confusion Matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
